da238651688fd059db397344d01d082d6455e2d0
lucasfsn <luki424221@gmail.com>
2025-11-05 12:51:53 +0100

    feat: enhance header and footer layout; add logo image and improve navigation links

--- frontend/src/app/shared/layouts/footer/footer.ts
+++ frontend/src/app/shared/layouts/footer/footer.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [MatIconModule, RouterLinkActive, RouterLink],
   templateUrl: './footer.html',
-  styleUrl: './footer.scss'
+  styleUrl: './footer.scss',
 })
-export class Footer {
-
-}
+export class Footer {}
