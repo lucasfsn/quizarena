d34634c80b7d2e6a3c0a6fd0fd2ce3b6719341ca
lucasfsn <luki424221@gmail.com>
2025-11-06 20:27:09 +0100

    fix: specify version for MapStruct dependencies

--- backend/QuizService/build.gradle.kts
+++ backend/QuizService/build.gradle.kts
@@ -59,8 +59,8 @@ repositories {
 }
 
 dependencies {
-	implementation("org.mapstruct:mapstruct")
-	annotationProcessor("org.mapstruct:mapstruct-processor")
+	implementation("org.mapstruct:mapstruct:1.6.3")
+	annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation( "org.springframework.boot:spring-boot-starter-validation")
