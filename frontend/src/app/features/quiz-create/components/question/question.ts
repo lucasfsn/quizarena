2b99a36222efc2f9e54c949281d0a36c06ec8f44
lucasfsn <luki424221@gmail.com>
2025-11-23 11:38:44 +0100

    feat: implement quiz creation page with dynamic question handling and constraints

--- frontend/src/app/features/quiz-create/components/question/question.ts
+++ frontend/src/app/features/quiz-create/components/question/question.ts
@@ -1,10 +1,19 @@
+import { QUIZ_FORM_CONSTRAINTS } from '@/app/features/quiz-create/constants/quiz-form-consts';
+import { QuestionForm } from '@/app/features/quiz-create/types/quiz-form';
+import { QuizFormConsts } from '@/app/features/quiz-create/types/quiz-form-consts';
 import { Button } from '@/app/shared/components/button/button';
 import { Component, computed, input, output } from '@angular/core';
-import { FormArray, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Checkbox } from 'primeng/checkbox';
 import { FloatLabel } from 'primeng/floatlabel';
 import { InputText } from 'primeng/inputtext';
 import { Textarea } from 'primeng/textarea';
+
+export interface QuestionData {
+  form: FormGroup<QuestionForm>;
+  index: number;
+  totalCount: number;
+}
 
 @Component({
   selector: 'app-question',
@@ -13,15 +22,12 @@ import { Textarea } from 'primeng/textarea';
   styleUrl: './question.scss',
 })
 export class Question {
-  public minAnswers = input.required<number>();
-  public maxAnswers = input.required<number>();
-  public minQuestions = input.required<number>();
-  public questionForm = input.required<FormGroup>();
-  public questionIndex = input.required<number>();
-  public questionsCount = input.required<number>();
+  protected readonly limits: QuizFormConsts = QUIZ_FORM_CONSTRAINTS;
+
+  public data = input.required<QuestionData>();
   public addAnswer = output<void>();
   public removeAnswer = output<number>();
   public removeQuestion = output<void>();
 
-  protected answers = computed(() => this.questionForm().get('answers') as FormArray);
+  protected readonly answers = computed(() => this.data().form.controls.answers);
 }
--- frontend/src/app/features/quiz-create/quiz-create.ts
+++ frontend/src/app/features/quiz-create/quiz-create.ts
@@ -1,8 +1,11 @@
-import { Question } from '@/app/features/quiz-create/components/question/question';
+import { Question, QuestionData } from '@/app/features/quiz-create/components/question/question';
 import { Title } from '@/app/features/quiz-create/components/title/title';
+import { QUIZ_FORM_CONSTRAINTS } from '@/app/features/quiz-create/constants/quiz-form-consts';
+import { AnswerForm, QuestionForm, QuizForm } from '@/app/features/quiz-create/types/quiz-form';
+import { QuizFormConsts } from '@/app/features/quiz-create/types/quiz-form-consts';
 import { Button } from '@/app/shared/components/button/button';
-import { Component } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-quiz-create',
@@ -10,58 +13,67 @@ import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } fr
   templateUrl: './quiz-create.html',
   styleUrl: './quiz-create.scss',
 })
-export class QuizCreate {
-  protected readonly MIN_ANSWERS = 2;
-  protected readonly MAX_ANSWERS = 4;
-  protected readonly MIN_QUESTIONS = 1;
-  protected readonly MAX_QUESTIONS = 20;
+export class QuizCreate implements OnInit {
+  protected readonly limits: QuizFormConsts = QUIZ_FORM_CONSTRAINTS;
 
   private readonly formBuilder = new FormBuilder();
-  protected readonly form = this.formBuilder.group({
-    title: ['', [Validators.required, Validators.minLength(2)]],
-    questions: this.formBuilder.array([]),
+  protected readonly form = this.formBuilder.group<QuizForm>({
+    title: this.formBuilder.control('', {
+      validators: [Validators.required, Validators.minLength(this.limits.MIN_TEXT_LENGTH)],
+      nonNullable: true,
+    }),
+    questions: this.formBuilder.array<FormGroup<QuestionForm>>([]),
   });
-  protected readonly questions = this.form.controls.questions as FormArray;
-
-  public constructor() {
-    this.addQuestion();
-  }
 
   protected addQuestion(): void {
-    if (this.questions.length >= this.MAX_QUESTIONS) return;
+    if (this.form.controls.questions.length >= this.limits.MAX_QUESTIONS) return;
 
-    const q = this.formBuilder.group({
-      text: ['', [Validators.required, Validators.minLength(2)]],
-      answers: this.formBuilder.array([this.createAnswer(), this.createAnswer()]),
+    const question = this.formBuilder.group<QuestionForm>({
+      text: this.formBuilder.control('', {
+        validators: [Validators.required, Validators.minLength(this.limits.MIN_TEXT_LENGTH)],
+        nonNullable: true,
+      }),
+      answers: this.formBuilder.array<FormGroup<AnswerForm>>([
+        this.createAnswer(),
+        this.createAnswer(),
+      ]),
     });
 
-    this.questions.push(q);
+    this.form.controls.questions.push(question);
   }
 
   protected removeQuestion(index: number): void {
-    console.log(index);
-    if (this.questions.length <= this.MIN_QUESTIONS) return;
-    this.questions.removeAt(index);
+    if (this.form.controls.questions.length <= this.limits.MIN_QUESTIONS) return;
+    this.form.controls.questions.removeAt(index);
   }
 
-  protected createAnswer(text: string = '', isCorrect: boolean = false): FormGroup {
-    return this.formBuilder.group({
-      text: [text, Validators.required],
-      isCorrect: [isCorrect],
+  protected createAnswer(text: string = '', isCorrect: boolean = false): FormGroup<AnswerForm> {
+    return this.formBuilder.group<AnswerForm>({
+      text: this.formBuilder.control(text, {
+        validators: [Validators.required, Validators.minLength(this.limits.MIN_TEXT_LENGTH)],
+        nonNullable: true,
+      }),
+      isCorrect: this.formBuilder.control(isCorrect, { nonNullable: true }),
     });
   }
 
-  protected getAnswers(index: number): FormArray {
-    return this.questions.at(index).get('answers') as FormArray;
+  protected getQuestionData(form: FormGroup<QuestionForm>, index: number): QuestionData {
+    return {
+      form,
+      index,
+      totalCount: this.form.controls.questions.length,
+    };
   }
 
   protected addAnswer(qIndex: number): void {
-    this.getAnswers(qIndex).push(this.createAnswer());
+    const answers = this.form.controls.questions.at(qIndex).controls.answers;
+    if (answers.length >= this.limits.MAX_ANSWERS) return;
+    answers.push(this.createAnswer());
   }
 
   protected removeAnswer(qIndex: number, aIndex: number): void {
-    const answers = this.getAnswers(qIndex);
-    if (answers.length <= this.MIN_ANSWERS) return;
+    const answers = this.form.controls.questions.at(qIndex).controls.answers;
+    if (answers.length <= this.limits.MIN_ANSWERS) return;
     answers.removeAt(aIndex);
   }
 
@@ -75,7 +87,7 @@ export class QuizCreate {
     console.log(this.form.value);
   }
 
-  protected get questionControls(): FormGroup[] {
-    return this.questions.controls as FormGroup[];
+  public ngOnInit(): void {
+    this.addQuestion();
   }
 }
