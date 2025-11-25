e97cacfdee5d315079f82a76e88254e1685fe879
lucasfsn <luki424221@gmail.com>
2025-11-25 17:14:16 +0100

    feat: add game page, lobby UI, and types

---
+++ frontend/src/app/features/game/components/game-summary/game-summary.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-summary',
+  imports: [],
+  templateUrl: './game-summary.html',
+  styleUrl: './game-summary.scss'
+})
+export class GameSummary {
+
+}
