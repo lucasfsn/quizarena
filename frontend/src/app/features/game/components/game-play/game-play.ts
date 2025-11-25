392446b629a2ef1f08c20e5202740fc598bd2175
lucasfsn <luki424221@gmail.com>
2025-11-25 17:08:13 +0100

    feat: add multiplayer game pages and lobby UI

---
+++ frontend/src/app/features/game/components/game-play/game-play.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-play',
+  imports: [],
+  templateUrl: './game-play.html',
+  styleUrl: './game-play.scss'
+})
+export class GamePlay {
+
+}
