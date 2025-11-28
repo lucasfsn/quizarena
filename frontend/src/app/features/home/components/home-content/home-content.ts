b359b2c3719afc883afe8992eacc9081123c86e3
lucasfsn <luki424221@gmail.com>
2025-11-28 18:33:55 +0100

    chore: use Authorization service for quiz creation flow, update fe docker compose config

--- frontend/src/app/features/home/components/home-content/home-content.ts
+++ frontend/src/app/features/home/components/home-content/home-content.ts
@@ -1,10 +1,12 @@
+import { Authorization } from '@/app/core/auth/authorization';
 import { HomeCard } from '@/app/features/home/components/home-card/home-card';
 import { Button } from '@/app/shared/components/button/button';
 import { FriendsIcon } from '@/app/shared/components/svg/friends-icon';
 import { HomeQuestionImage } from '@/app/shared/components/svg/home-question-image';
 import { KnowledgeIcon } from '@/app/shared/components/svg/knowledge-icon';
 import { RankingIcon } from '@/app/shared/components/svg/ranking-icon';
-import { Component, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-content',
@@ -13,6 +15,15 @@ import { Component, signal } from '@angular/core';
   styleUrl: './home-content.scss',
 })
 export class HomeContent {
-  protected readonly isLoggedIn = signal<boolean>(true);
-  protected readonly isLoading = signal<boolean>(false);
+  private authorizationService = inject(Authorization);
+  private router = inject(Router);
+
+  protected isLoggedIn(): boolean {
+    return this.authorizationService.isLoggedIn();
+  }
+
+  protected handleLeftButtonClick(): void {
+    if (this.isLoggedIn()) this.router.navigate(['/quizzes/new']);
+    else this.authorizationService.login();
+  }
 }
