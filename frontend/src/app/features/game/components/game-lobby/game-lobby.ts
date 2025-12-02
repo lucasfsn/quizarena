fdb666986b54529516aff962aa677940fd7d3139
lucasfsn <luki424221@gmail.com>
2025-12-02 17:11:02 +0100

    feat: revamp game lobby UI and add copy room code

--- frontend/src/app/features/game/components/game-lobby/game-lobby.ts
+++ frontend/src/app/features/game/components/game-lobby/game-lobby.ts
@@ -1,14 +1,24 @@
+import { Toast } from '@/app/core/services/toast/toast';
 import { LobbyPayload } from '@/app/features/game/types/lobby';
 import { Button } from '@/app/shared/components/button/button';
 import { GameLobbyImage } from '@/app/shared/components/svg/game-lobby-image';
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
+import { Carousel } from 'primeng/carousel';
 
 @Component({
   selector: 'app-game-lobby',
-  imports: [Button, GameLobbyImage],
+  imports: [Button, GameLobbyImage, Carousel],
   templateUrl: './game-lobby.html',
   styleUrl: './game-lobby.scss',
 })
 export class GameLobby {
   public game = input.required<LobbyPayload>();
+
+  private toastService = inject(Toast);
+
+  protected copyRoomCode(gameCode: string): void {
+    navigator.clipboard
+      .writeText(gameCode)
+      .then(() => this.toastService.success('Copied to clipboard.'));
+  }
 }
