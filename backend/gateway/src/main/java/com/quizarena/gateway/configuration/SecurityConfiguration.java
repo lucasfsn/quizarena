806f772385cda9af519d014d0a29004df0917ba7
Michał Witkowski <97167650+emWitkowsky@users.noreply.github.com>
2025-12-30 19:45:14 +0100

    Feat: Setup usermanagement and Keycloak (#12)
    * setup project for user management
    
    * user management service and keycloak config created
    
    * past work
    
    * change setup for keycloak
    
    * remove keyloak from docker setup

---
+++ backend/gateway/src/main/java/com/quizarena/gateway/configuration/SecurityConfiguration.java
@@ -0,0 +1,102 @@
+package com.quizarena.gateway.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtTimestampValidator;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfiguration {
+
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+
+        http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/hello-world").permitAll()
+                        .pathMatchers("/**").permitAll()
+                        // .anyExchange().authenticated()
+                        .anyExchange().permitAll()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt.jwtAuthenticationConverter(new JWTAuthConverter()))
+                );
+
+        return http.build();
+
+//        http
+////                .csrf(AbstractHttpConfigurer::disable)
+//                .csrf(csfr -> csfr.disable())
+//                .authorizeHttpRequests(auth -> auth
+//                                .requestMatchers("/**").permitAll()
+//                                .requestMatchers("/hello-world").permitAll()
+//                                .requestMatchers(HttpMethod.POST, "/**").permitAll()
+////                        .anyRequest().authenticated()
+//                                .anyRequest().permitAll() // so far we let that say
+//                )
+//                .oauth2ResourceServer(oauth2 -> oauth2
+//                        .jwt(jwt -> jwt
+//                                .jwtAuthenticationConverter(new JWTAuthConverter())
+//                        )
+//                )
+//                .sessionManagement(session -> session
+//                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                );
+
+//        return http.build();
+    }
+
+
+
+    //Spring Clound Gateway does it declaratively in yaml - we are NOT using servlets
+//    private CorsConfigurationSource corsConfigurationSource() {
+//        return request -> {
+//            CorsConfiguration configuration = new CorsConfiguration();
+//            configuration.setAllowedOrigins(List.of("*"));
+//            configuration.setAllowedMethods(List.of("*"));
+//            configuration.setAllowedHeaders(List.of("*"));
+//            return configuration;
+//        };
+//    }
+
+//    @Bean
+//    public JwtDecoder jwtDecoder() {
+//        return NimbusJwtDecoder.withJwkSetUri("http://keycloak:8080/realms/quizarena/protocol/openid-connect/certs").build();
+//    }
+
+    @Bean
+    public ReactiveJwtDecoder jwtDecoder() {
+        NimbusReactiveJwtDecoder jwtDecoder = NimbusReactiveJwtDecoder.withJwkSetUri("http://keycloak:8080/realms/quizarena/protocol/openid-connect/certs").build();
+
+        // Create a validator that ONLY checks the expiration time, not the issuer string
+        OAuth2TokenValidator<Jwt> withTimestamp = new JwtTimestampValidator();
+
+        // We do NOT add JwtIssuerValidator here to avoid the string mismatch error
+        OAuth2TokenValidator<Jwt> validator = new DelegatingOAuth2TokenValidator<>(withTimestamp);
+
+        jwtDecoder.setJwtValidator(validator);
+        return jwtDecoder;
+    }
+}
