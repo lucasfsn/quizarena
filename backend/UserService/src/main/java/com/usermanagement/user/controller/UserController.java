65f612a4d0f7b6c7ed02b3d887b36baebbda6535
Michał Witkowski <m.witkowski.711@studms.ug.edu.pl>
2026-01-04 19:51:07 +0100

    add responseDto logic and test spring security token passage

--- backend/UserService/src/main/java/com/usermanagement/user/controller/UserController.java
+++ backend/UserService/src/main/java/com/usermanagement/user/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.usermanagement.user.controller;
 
 // import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +15,13 @@ public class UserController {
     @GetMapping
     public String hello() {
         return "unprotected";
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<?> test(@AuthenticationPrincipal Jwt jwt) {
+        String userId = jwt.getSubject(); // ID użytkownika z Keycloak
+        String email = jwt.getClaimAsString("email");
+        return ResponseEntity.ok("User ID: " + userId + ", Email: " + email);
     }
 
     @GetMapping("/sec")
