a613ec87ee749b9afb4513f3bc6dd1e2914a5245
Michał Witkowski <m.witkowski.711@studms.ug.edu.pl>
2026-01-18 18:32:15 +0100

    delete obsolete code

--- backend/UserService/src/main/java/com/usermanagement/configuration/KeycloakClient.java
+++ backend/UserService/src/main/java/com/usermanagement/configuration/KeycloakClient.java
@@ -2,20 +2,19 @@ package com.usermanagement.configuration;
 
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.KeycloakBuilder;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class KeycloakClient {
 
-    @Value("${app.keycloak.admin.clientId}")
+//    @Value("${app.keycloak.admin.clientId}")
     private String clientId;
-    @Value("${app.keycloak.admin.clientSecret}")
+//    @Value("${app.keycloak.admin.clientSecret}")
     private String clientSecret;
-    @Value("${app.keycloak.realm}")
+//    @Value("${app.keycloak.realm}")
     private String realm;
-    @Value("${app.keycloak.serverUrl}")
+//    @Value("${app.keycloak.serverUrl}")
     private String serverUrl;
 
     @Bean
