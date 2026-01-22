fa9576fa9725ce7b7632b9f827f840f4fbc8ecd7
lucasfsn <luki424221@gmail.com>
2026-01-22 21:39:48 +0100

    fix: add missing files

--- frontend/src/app/features/leaderboard/pages/leaderboard/leaderboard.ts
+++ frontend/src/app/features/leaderboard/pages/leaderboard/leaderboard.ts
@@ -14,6 +14,7 @@ import { lastValueFrom } from 'rxjs';
   selector: 'app-leaderboard',
   imports: [Button, FallbackUi, FetchErrorImage, LeaderboardList],
   templateUrl: './leaderboard.html',
+  styleUrl: './leaderboard.scss',
 })
 export class Leaderboard {
   private readonly leaderboardService = inject(LeaderboardService);
